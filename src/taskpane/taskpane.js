a076e402b1405e243f00249c646f89992f1314aa
dean-vallas <dean.vallas@hotmail.com>
2021-03-18 19:41:44 -0700

    closer to complete script import

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -17,6 +17,7 @@
                 $("#button-desc").text("Display the selected text");
                 return;
             }
+
             //displayAllBindings();  
             //Office.context.document.settings.set("Office.AutoShowTaskpaneWithDocument", true);
             Office.context.document.settings.saveAsync();
@@ -24,6 +25,8 @@
 
             // #region Click Events;
             // Add event handlers for each button.
+
+            $("#fileInput").change(fileInput_change);
             $("#btnListCharNames").click(btnListCharNames);
 
             //$('#selectName').change(selectNameChanged);
@@ -143,6 +146,28 @@
     }
 
     // #region Buttons
+    function fileInput_change() {
+        $("#divTopMessage").html("adding change handler");
+
+        $("#divSelectName").hide();
+        $("#divTopMessage").html("");
+        //$("#divUserMessage").html("");
+        $("#Write").hide();
+        $("#displayDiv").html("");
+        $("#divTopMessage").html("adding change handler");
+        var file = document.getElementById("fileInput").files[0];
+        $("#divTopMessage").html("getting file");
+        var reader = new FileReader()
+        reader.onload = function (e) {
+            var xml = reader.result;
+            $("#divTopMessage").html(xml);
+            processXml(xml)
+            $("#divTopMessage").html("calling initiateImport with xml");
+            CreateImportedScript(initiateImport(xml))
+        }
+        reader.readAsText(file)
+    }
+
 
     function btnComms_click() {
         $("#write").hide();
@@ -1468,4 +1493,123 @@
             ctx.lineTo(pts[i], pts[i + 1]);
     }
     // #endregion
+
+    // #region Import Final Draft
+
+    //entry point to get parsed array of elements and values
+    //called when file input element changes (gets a file)
+    function initiateImport(xml) {
+        return processXml(xml);
+    }
+
+    //incoming xml argument contains the whole collection of script paragraphs
+    function processXml(xml) {
+        var parser, xmlDoc
+        parser = new DOMParser()
+        xmlDoc = parser.parseFromString(xml, 'text/xml')
+        var paragraphs = xmlDoc.getElementsByTagName('Paragraph')
+        let output = [],
+            scriptElementType,
+            attributeText,
+            textAttribArrayForElement
+
+        //the outer loop through the script
+        for (let i = 0; i < paragraphs.length; i++) {
+            scriptElementType = '',
+                attributeText = ''//,
+
+            if (paragraphs[i].getAttributeNode('Type'))
+                scriptElementType = paragraphs[i].getAttributeNode('Type')
+
+            if (paragraphs[i].getElementsByTagName('Text')) {
+                textAttribArrayForElement = paragraphs[i].getElementsByTagName('Text')
+                for (let j = 0; j < textAttribArrayForElement.length; j++) {
+                    if (textAttribArrayForElement[j].childNodes &&
+                        textAttribArrayForElement[j].childNodes.length > 0)
+                        attributeText += textAttribArrayForElement[j].childNodes[0].nodeValue
+                }
+            }
+
+            if (typeof scriptElementType == 'undefined') scriptElementType = "empty";
+            if (typeof attributeText == 'undefined') attributeText = "blank";
+            output.push(buildString(scriptElementType.nodeValue, attributeText));
+        }
+        console.log(output)
+        return output
+
+    }
+
+    async function buildString(scriptElementType, attributeText) {
+        let out = []
+
+        switch (scriptElementType) {
+            case 'Scene Heading':
+                out.push('Slugline', attributeText)
+                break
+            case 'Action':
+                out.push('Action', attributeText)
+                break
+            case 'Character':
+                out.push('Character Name', attributeText)
+                break
+            case 'Dialogue':
+                out.push('Dialog', attributeText)
+                break
+            case 'Parenthetical':
+                out.push('Parenthetical', attributeText)
+                break
+            case 'End of Act':
+                out.push('Act Break', attributeText)
+                break
+            case 'General':
+                out.push('General', attributeText)
+                break
+            // default:
+            //     try {
+            //         out.push(
+            //             scriptElementType + ', ' + attributeText
+            //         )
+            //     } catch {
+            //         out.push('Error')
+            //     }
+            //     break
+        }
+        return out
+    }
+    function CreateImportedScript(ParagraphArray) {
+        $("#divTopMessage").html("about to write to the new page" + ParagraphArray);
+
+        var i;
+        Word.run(async (context) => {
+            for (i = 0; i < ParagraphArray.length; i++) {
+                context.document.body.insertParagraph(ParagraphArray[i][1], Word.InsertLocation.end);
+                return context.sync();
+            }
+
+            $("#divTopMessage").html("finished first loop.  i=" + i);
+
+            var NewParas = context.document.body.paragraphs;
+            context.load(NewParas, "items, style");
+
+            for (let j = 0; j < ParagraphArray.length; j++) {
+                NewParas.items[j].style = ParagraphArray[i][0];
+                $("#divTopMessage").html("style for para " + j);
+            }
+            context.sync()
+                .then(function () {
+                    console.log("Finished para");
+                })
+        }).catch($("#divTopMessage").html("finished writing new script" + error.message))
+
+
+
+        // $("#divTopMessage").html(error);
+        // return errorHandler(error);
+
+    }
+
+
+
+    // #endregion
 })();
+
